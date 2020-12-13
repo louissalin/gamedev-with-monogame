d814a4ef541407f9c6f3b151f91418cbf6b713e3
Louis Salin <louis.phil@gmail.com>
2020-12-13 11:49:03 -0600

    show rolling min/max FPS

--- Engine/Objects/StatsObject.cs
+++ Engine/Objects/StatsObject.cs
@@ -6,11 +6,13 @@ namespace Engine2D.Objects
 {
     public class StatsObject : BaseTextObject
     {
-        public const int MAXIMUM_SAMPLES = 100;
+        public const int ROLLING_SIZE = 60;
 
-        private Queue<float> _sampleBuffer = new Queue<float>();
+        private Queue<float> _rollingFPS = new Queue<float>();
 
         public float FPS { get; set; }
+        public float MinFPS { get; private set; }
+        public float MaxFPS { get; private set; }
         public float AverageFPS { get; private set; }
         public bool IsRunningSlowly { get; set; }
         public int NbUpdateCalled { get; set; }
@@ -27,25 +29,40 @@ namespace Engine2D.Objects
             NbUpdateCalled++;
             FPS = 1.0f / (float) gameTime.ElapsedGameTime.TotalSeconds;
 
-            _sampleBuffer.Enqueue(FPS);
+            _rollingFPS.Enqueue(FPS);
 
-            if (_sampleBuffer.Count > MAXIMUM_SAMPLES)
+            if (_rollingFPS.Count > ROLLING_SIZE)
             {
-                _sampleBuffer.Dequeue();
+                _rollingFPS.Dequeue();
 
                 var sum = 0.0f;
-                foreach (var fps in _sampleBuffer.ToArray())
+                MaxFPS = int.MinValue;
+                MinFPS = int.MaxValue;
+                foreach (var fps in _rollingFPS.ToArray())
                 {
                     sum += fps;
+                    if (fps > MaxFPS)
+                    {
+                        MaxFPS = fps;
+                    }
+
+                    if (fps < MinFPS)
+                    {
+                        MinFPS = fps;
+                    }
                 }
-                AverageFPS = sum / _sampleBuffer.Count;
+                AverageFPS = sum / _rollingFPS.Count;
             }
             else
             {
                 AverageFPS = FPS;
+                MinFPS = FPS;
+                MaxFPS = FPS;
             }
 
             Text = $"FPS: {FPS}" + System.Environment.NewLine;
+            Text += $"Min FPS: {MinFPS}" + System.Environment.NewLine;
+            Text += $"Max FPS: {MaxFPS}" + System.Environment.NewLine;
             Text += $"Avg FPS: {AverageFPS}" + System.Environment.NewLine;
             Text += $"Running Slowly: {IsRunningSlowly}" + System.Environment.NewLine;
             Text += $"Nb Updates: {NbUpdateCalled}" + System.Environment.NewLine;
